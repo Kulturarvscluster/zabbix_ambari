a21146ff5c0835fcf20f78965cb1caa3ef761dcb
Asger Askov Blekinge <abr@kb.dk>
2018-04-23 14:42:28 +0200

    Note about selinux

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -x
 
 SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 
-HOST=${1:-root@kac-proj-000}
+HOST=${1:-root@kac-abri-001}
 
 cd $SCRIPT_DIR
 
@@ -17,8 +17,10 @@ ssh $HOST chown zabbix:zabbix /var/lib/zabbix/ambari_lld.py
 
 #Create a file `/var/lib/zabbix/.netrc` with this content and set permissions to 0700
 
-grep abri ~/.netrc | xargs -r -I{} ssh $HOST " ( cat /var/lib/zabbix/.netrc | grep '{}') || (echo '{}' | cat > /var/lib/zabbix/.netrc ) ; chmod go-rwx /var/lib/zabbix/.netrc; chown zabbix:zabbix /var/lib/zabbix/.netrc"
+grep zambarix ~/.netrc | xargs -r -I{} ssh $HOST " ( cat /var/lib/zabbix/.netrc | grep '{}') || (echo '{}' | cat > /var/lib/zabbix/.netrc ) ; chmod go-rwx /var/lib/zabbix/.netrc; chown zabbix:zabbix /var/lib/zabbix/.netrc"
 
 scp userparameter_ambari_alerts.conf $HOST:/etc/zabbix/zabbix_agentd.d/
 
 ssh $HOST systemctl restart zabbix-agent
+
+echo "You must disable selinux or install a module to allow zabbix to make http requests"
